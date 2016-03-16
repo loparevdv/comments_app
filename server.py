a67aca811bd4f9183c3ef2ceda447876c8690ad3
loparevdv <loparevdv@gmail.com>
2016-03-16 14:59:00 +0300

    very naive prototype

--- server.py
+++ server.py
@@ -1,51 +1,86 @@
-
-from urllib.parse import urlparse, parse_qsl
-
-import aiohttp
-import aiohttp.server
+# import aiohttp.server
 import aiopg
-from aiohttp import MultiDict
-
 import asyncio
 import aiopg
 
-@asyncio.coroutine
-def get_comments():
-    conn = yield from aiopg.connect(database='comment_app',
-                                    user='postgres',
-                                    password='postgres',
-                                    host='127.0.0.1')
-    cur = yield from conn.cursor()
-    yield from cur.execute("SELECT * FROM comment")
-    ret = yield from cur.fetchall()
-    return ret
+from aiohttp import web
+from urllib.parse import urlparse, parse_qsl
+
+# TODO:
+# Define URLS, Comment.API, etc...
+# Tests!
+# ...
+
+DB_ARGS = {
+    'database': 'comment_app',
+    'user': 'postgres',
+    'password': 'postgres',
+    'host': '127.0.0.1'
+}
 
 
-class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
+class Comment(object):
 
-    async def handle_request(self, message, payload):
-        response = aiohttp.Response(
-            self.writer, 200, http_version=message.version
-        )
-        res = await get_comments()
-        data = str(res)
-        response.add_header('Content-Type', 'text/html')
-        response.add_header('Content-Length', str(len(data)))
-        response.send_headers()
-        response.write(data.encode('utf-8'))
-        await response.write_eof()
+    @classmethod
+    async def get_all(cls):
+        connection = await aiopg.connect(**DB_ARGS)
+        cursor = await connection.cursor()
 
+        await cursor.execute("SELECT * FROM comment")
+        return await cursor.fetchall()
 
-if __name__ == '__main__':
+    @classmethod
+    async def get(cls, comment_id):
+        connection = await aiopg.connect(**DB_ARGS)
+        cursor = await connection.cursor()
 
-    loop = asyncio.get_event_loop()
-    f = loop.create_server(lambda: HttpRequestHandler(debug=True, keep_alive=75), '0.0.0.0', '8080')
-    srv = loop.run_until_complete(f)
+        await cursor.execute("SELECT * FROM comment WHERE id=%s" % comment_id)
+        return await cursor.fetchall()
 
-    print('serving on', srv.sockets[0].getsockname())
+    @classmethod
+    async def get_thread(cls, root_id):
+        connection = await aiopg.connect(**DB_ARGS)
+        cursor = await connection.cursor()
 
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
+        query = 'SELECT id, parent_id, comment_text, created, modified \
+                 FROM comments_relations JOIN comment \
+                 ON comments_relations.ancestor_id = comment.id \
+                 WHERE ancestor_id= %s'
+
+        await cursor.execute(query % root_id)
+        return await cursor.fetchall()
+
+    def create(cls):
         pass
 
+    def update(cls):
+        pass
+
+    def delete(cls):
+        pass
+
+
+async def index(request):
+    res = await Comment.get_all()
+    data = str(res).encode('utf-8')
+    return web.Response(body=data)
+
+async def comment(request):
+    comment_id = request.match_info['comment_id']
+    res = await Comment.get(comment_id)
+    data = str(res).encode('utf-8')
+    return web.Response(body=data)
+
+async def branch(request):
+    root_id = request.match_info['root_id']
+    res = await Comment.get_thread(root_id)
+    data = str(res).encode('utf-8')
+    return web.Response(body=data)
+
+
+app = web.Application()
+app.router.add_route('GET', '/', index)
+app.router.add_route('GET', '/{comment_id}/', comment)
+app.router.add_route('GET', '/branch/{root_id}/', branch)
+
+web.run_app(app)
