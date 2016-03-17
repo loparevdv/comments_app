44b882a2b8460a1bde8c0f0c165c2d695a4f68dc
loparevdv <loparevdv@gmail.com>
2016-03-17 12:21:48 +0300

    connection pool used

--- server.py
+++ server.py
@@ -22,38 +22,27 @@ DB_ARGS = {
 class Comment(object):
 
     @classmethod
-    async def get_all(cls):
-        connection = await aiopg.connect(**DB_ARGS)
-        cursor = await connection.cursor()
+    async def get(cls, request, comment_id):
+        query = 'SELECT * FROM comment WHERE id=%s'
 
-        await cursor.execute("SELECT * FROM comment")
-        return await cursor.fetchall()
-
-    @classmethod
-    async def get(cls, comment_id):
-        connection = await aiopg.connect(**DB_ARGS)
-        cursor = await connection.cursor()
-
-        await cursor.execute('SELECT * FROM comment WHERE id=%s' % comment_id)
-        return await cursor.fetchall()
+        with (request.cursor) as cur:
+            await cur.execute(query % (comment_id, ))
+            return await cur.fetchall()
 
     @classmethod
-    async def get_thread(cls, root_id):
-        connection = await aiopg.connect(**DB_ARGS)
-        cursor = await connection.cursor()
+    async def get_thread(cls, request, root_id):
 
         query = 'SELECT id, parent_id, comment_text, created, modified \
                  FROM comments_relations JOIN comment \
-                 ON comments_relations.ancestor_id = comment.id \
+                 ON comments_relations.descendant_id = comment.id \
                  WHERE ancestor_id= %s'
 
-        await cursor.execute(query % root_id)
-        return await cursor.fetchall()
+        with (request.cursor) as cur:
+            await cur.execute(query % (root_id, ))
+            return await cur.fetchall()
 
     @classmethod
-    async def create(cls, parent_id, text):
-        connection = await aiopg.connect(**DB_ARGS)
-        cursor = await connection.cursor()
+    async def create(cls, request, parent_id, text):
 
         if int(parent_id):
             query = 'INSERT INTO comment (parent_id, comment_text) VALUES (%s, %s)'
@@ -61,11 +50,17 @@ class Comment(object):
         else:
             query = 'INSERT INTO comment (comment_text) VALUES (%s)'
             args = (text, )
+        
+        with (request.cursor) as cur:
+            return await cur.execute(query % args)
 
-        return await cursor.execute(query % args)
+    @classmethod
+    async def update(cls, request, comment_id, comment_text):
+        query = 'update comment set comment_text = %s where id = %d;'
+        args = (comment_text, int(comment_id))
 
-    def update(cls):
-        pass
+        with (request.cursor) as cur:
+            return await cur.execute(query % args)
 
     def delete(cls):
         pass
@@ -76,11 +71,6 @@ class Comment(object):
         cursor = await connection.cursor()
 
 
-async def index(request):
-    res = await Comment.get_all()
-    data = str(res).encode('utf-8')
-    return web.Response(body=data)
-
 async def comment(request):
     comment_id = request.match_info['comment_id']
     res = await Comment.get(comment_id)
@@ -89,26 +79,42 @@ async def comment(request):
 
 async def branch(request):
     root_id = request.match_info['root_id']
-    res = await Comment.get_thread(root_id)
+    res = await Comment.get_thread(request, root_id)
     data = str(res).encode('utf-8')
     return web.Response(body=data)
 
 async def create(request):
     post = await request.post()
-    res = await Comment.create(post['parent_id'], post['comment_text'])
+    res = await Comment.create(request, post['parent_id'], post['comment_text'])
     data = str(res).encode('utf-8')
     return web.Response(body=data)
 
 async def update(request):
-    root_id = request.match_info['root_id']
+    comment_id = request.match_info['comment_id']
+    post = await request.post()
+    res = await Comment.update(request, comment_id, post['comment_text'])
+    data = str(res).encode('utf-8')
+    return web.Response(body=data)
 
 async def delete(request):
     # REQ - only comments without descendants can be deleted
     root_id = request.match_info['root_id']
 
 
-app = web.Application()
-app.router.add_route('GET', '/', index)
+async def middleware_factory(app, handler):
+
+    async def middleware_handler(request):
+        dsn = 'dbname=comment_app user=postgres password=postgres host=localhost port=5432'
+        pool = await aiopg.create_pool(dsn)
+        cursor = await pool.cursor()
+        request.cursor = cursor
+        return await handler(request)
+
+    return middleware_handler
+
+
+app = web.Application(middlewares=[middleware_factory])
+
 app.router.add_route('GET', '/{comment_id}/', comment)
 app.router.add_route('POST', '/create/', create)
 app.router.add_route('POST', '/update/{comment_id}/', update)
