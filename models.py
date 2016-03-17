024d072c81d36369013c508d4f4255c2ccf12fd3
loparevdv <loparevdv@gmail.com>
2016-03-17 22:16:10 +0300

    entity comments with descs api added

--- models.py
+++ models.py
@@ -5,26 +5,12 @@ class Comment(object):
     @classmethod
     async def get(cls, request, comment_id):
         query = 'SELECT * FROM comment WHERE id = %s'
-
         cur = await request.pool.cursor()
-        await cur.execute(query % (comment_id, ))
-        return await cur.fetchall()
-
-    @classmethod
-    async def get_thread(cls, request, root_id):
-
-        query = 'SELECT id, parent_id, comment_text, created, modified \
-                 FROM comments_relations JOIN comment \
-                 ON comments_relations.descendant_id = comment.id \
-                 WHERE ancestor_id= %s'
-
-        cur = await request.pool.cursor()
-        await cur.execute(query % (root_id, ))
+        await cur.execute(query, (comment_id, ))
         return await cur.fetchall()
 
     @classmethod
     async def create(cls, request, parent_id, text):
-
         if int(parent_id):
             query = 'INSERT INTO comment (parent_id, comment_text) VALUES (%s, %s)'
             args = (parent_id, text)
@@ -33,34 +19,54 @@ class Comment(object):
             args = (text, )
         
         cur = await request.pool.cursor()
-        return await cur.execute(query % args)
+        return await cur.execute(query, args)
 
     @classmethod
     async def update(cls, request, comment_id, comment_text):
         cur = await request.pool.cursor()
         query = 'UPDATE comment SET comment_text = %s WHERE id = %d;'
         args = (comment_text, int(comment_id))
-        return await cur.execute(query % args)
-
-    @classmethod
-    async def get_descendants_count(cls, request, comment_id):
-        cur = await request.pool.cursor()
-        query = 'SELECT count(*) FROM comments_relations WHERE ancestor_id = %d;'
-        await cur.execute(query % (int(comment_id), ))
-        res = await cur.fetchone()
-        return res
-
-    @classmethod
-    async def _flush_relations(cls, request, comment_id):
-        cur = await request.pool.cursor()
-        query = 'DELETE FROM comments_relations WHERE ancestor_id = %d OR descendant_id = %d'
-        await cur.execute(query % (int(comment_id), int(comment_id), ))
-        res = cur.fetchone()
-        return res
+        return await cur.execute(query, args)
 
     @classmethod
     async def delete(cls, request, comment_id):
         await cls._flush_relations(request, comment_id)
         cur = await request.pool.cursor()
         query = 'DELETE FROM comment WHERE id = %d'
-        return await cur.execute(query % (int(comment_id), ))
+        return await cur.execute(query, (int(comment_id), ))
+
+    @classmethod
+    async def _flush_relations(cls, request, comment_id):
+        cur = await request.pool.cursor()
+        query = 'DELETE FROM comments_relations WHERE ancestor_id = %d OR descendant_id = %d'
+        await cur.execute(query, (int(comment_id), int(comment_id), ))
+        return await cur.fetchone()
+
+    @classmethod
+    async def get_descendants_count(cls, request, comment_id):
+        cur = await request.pool.cursor()
+        query = 'SELECT count(*) FROM comments_relations WHERE ancestor_id = %d;'
+        await cur.execute(query, (int(comment_id), ))
+        return await cur.fetchone()
+
+    @classmethod
+    async def get_thread(cls, request, root_id):
+        query = 'SELECT id, parent_id, comment_text, created, modified \
+                 FROM comments_relations JOIN comment \
+                 ON comments_relations.descendant_id = comment.id \
+                 WHERE ancestor_id= %s'
+
+        cur = await request.pool.cursor()
+        await cur.execute(query, (root_id, ))
+        return await cur.fetchall()
+
+    @classmethod
+    async def get_entity_thread(cls, request, root_content_type, root_id):
+        query = 'SELECT id, parent_id, comment_text, created, modified \
+        FROM comments_relations JOIN comment \
+        ON comments_relations.descendant_id = comment.id \
+        WHERE ancestor_id IN (SELECT id from comment where root_content_type = %s and root_id = %s);'
+
+        cur = await request.pool.cursor()
+        await cur.execute(query, (root_content_type, root_id))
+        return await cur.fetchall()
--- server.py
+++ server.py
@@ -6,14 +6,9 @@ from middlewares import middleware_factory
 from models import Comment
 
 
-async def comment(request):
+async def get_one(request):
     comment_id = request.match_info['comment_id']
     res = await Comment.get(request, comment_id)
-    return web.Response(body=str(res).encode('utf-8'))
-
-async def branch(request):
-    root_id = request.match_info['root_id']
-    res = await Comment.get_thread(request, root_id)
     return web.Response(body=str(res).encode('utf-8'))
 
 async def create(request):
@@ -30,22 +25,33 @@ async def update(request):
 async def delete(request):
     comment_id = request.match_info['comment_id']
     res = await Comment.get_descendants_count(request, comment_id)
-
     desc_count = res[0] - 1
-    # desc_count = 0
     # REQ - only comments without descendants can be deleted
     if desc_count == 0:
         await Comment.delete(request, comment_id)
 
     return web.Response(body=str(desc_count).encode('utf-8'))
 
+async def branch(request):
+    root_id = request.match_info['root_id']
+    res = await Comment.get_thread(request, root_id)
+    return web.Response(body=str(res).encode('utf-8'))
+
+async def ent_branch(request):
+    root_id = request.match_info['root_id']
+    root_ct_id = request.match_info['root_content_type']
+    res = await Comment.get_entity_thread(request, root_ct_id, root_id)
+    return web.Response(body=str(res).encode('utf-8'))
+
 app = web.Application(middlewares=[middleware_factory])
 
-app.router.add_route('GET', '/{comment_id}/', comment)
+app.router.add_route('GET', '/{comment_id}/', get_one)
 app.router.add_route('POST', '/create/', create)
 app.router.add_route('POST', '/update/{comment_id}/', update)
 app.router.add_route('DELETE', '/delete/{comment_id}/', delete)
 
+# TODO: rename to thread?
 app.router.add_route('GET', '/branch/{root_id}/', branch)
+app.router.add_route('GET', '/ent_branch/{root_content_type}/{root_id}/', ent_branch)
 
 web.run_app(app)
